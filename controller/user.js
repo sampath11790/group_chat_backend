9b05f33302ac334eec395e2cae03b441da5ef2b6
sampath11790 <bsampath11790@gmail.com>
2023-06-15 15:06:28 +0530

    Added login logic, password comparison, and verification using bcrypt.

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,25 @@
 const bcrypt = require("bcrypt");
 const User = require("../model/user");
-exports.userLogin = (req, res, next) => {
+exports.userLogin = async (req, res, next) => {
   try {
+    const obj = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const user = await User.findAll({ where: { email: obj.email } });
+    if (user.length > 0) {
+      //do somthing
+      const isMatch = await bcrypt.compare(obj.password, user[0].password);
+      if (!isMatch) {
+        //reject
+        throw new Error();
+      } else {
+        // send response
+        res.status(200).json({ message: "signup successful" });
+      }
+    } else {
+      throw new Error();
+    }
   } catch (err) {
     res.status(401).json({ error: "enter valid data" });
   }
