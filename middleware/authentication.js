e98b4c5b04d885ba58cecdcb5acdd8a5ab682cc6
sampath11790 <bsampath11790@gmail.com>
2023-06-17 08:58:58 +0530

    JWT token verify function

---
+++ middleware/authentication.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const TokenValidation = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    //vefify token
+    const response = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    //finding user data and adding in to request object
+    const Userdata = await User.findByPk(response.userId);
+    if (Userdata) {
+      req.user = Userdata;
+      next();
+    }
+  } catch (err) {
+    res.status(401).json({
+      error: "yor are not authorized to acceess this page",
+    });
+  }
+};
+module.exports = TokenValidation;
