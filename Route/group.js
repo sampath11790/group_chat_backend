cbbf8291c028427ba862f5419d84230adc8cba5c
sampath11790 <bsampath11790@gmail.com>
2023-06-18 11:05:19 +0530

    adding new member into group

--- Route/group.js
+++ Route/group.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const TokenValidation = require("../middleware/authentication");
-const { creategroup } = require("../controller/group");
+const { creategroup, adduser } = require("../controller/group");
 
 const route = express.Router();
 
 route.post("/group/create", TokenValidation, creategroup);
-route.post("/group/adduser", TokenValidation, () => {});
+route.post("/group/adduser", TokenValidation, adduser);
 route.post("/group/join", TokenValidation, () => {});
 
 module.exports = route;
--- controller/group.js
+++ controller/group.js
@@ -1,3 +1,5 @@
+const User = require("../model/user");
+
 exports.creategroup = (req, res, next) => {
   try {
     // console.log("creategroup");
@@ -8,5 +10,29 @@ exports.creategroup = (req, res, next) => {
     res.status(200).json({ message: "group created  successfull" });
   } catch (err) {
     res.status(401).json({ error: "this group is already exist " });
+  }
+};
+
+exports.adduser = async (req, res, next) => {
+  try {
+    // console.log("creategroup");
+    const groupid = req.body.id;
+    const groupmember = await User.findAll({
+      where: { email: req.body.email },
+    });
+    // console.log(groupmember);
+    const group = await req.user.getGroups({ where: { id: groupid } });
+    // console.log(group);
+
+    if (!groupmember.length || !group.length) {
+      return res.status(404).json({ message: "User or group not found" });
+    }
+    console.log(group);
+    await group[0].addUser(groupmember[0], { through: { admin: false } });
+
+    res.status(200).json({ message: "member successfully added in group" });
+  } catch (err) {
+    // console.log(err);
+    res.status(401).json({ error: "error ", message: err });
   }
 };
